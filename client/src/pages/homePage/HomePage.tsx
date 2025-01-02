3f209bf03f50ec460d739c77b9505a2ce14195e2
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-01-02 22:12:35 +0100

    feat: accueil intrasense S01US26

--- client/src/pages/homePage/HomePage.tsx
+++ client/src/pages/homePage/HomePage.tsx
@@ -3,7 +3,6 @@ import style from "./homePage.module.css";
 export default function HomePage() {
   return (
     <div>
-      {/* <Navbar /> */}
       <main>
         <button type="button" className={style.buttonCreateDecision}>
           Créer une prise de décision
@@ -11,43 +10,24 @@ export default function HomePage() {
 
         <section>
           <h2>Les décisions où je participe</h2>
-
           <div className={style.cardsContainer}>
-            {/* map des decisions */}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nam
-            omnis tempore cum quos cupiditate porro alias ducimus facilis
-            quibusdam a similique quas quis enim deserunt, velit neque
-            reprehenderit excepturi.
+            cards : Les décisions où je participe
           </div>
         </section>
-
         <section>
           <h2>Mes décisions</h2>
-          <div className={style.cardsContainer}>
-            {/* map des decisions */}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nam
-            omnis tempore cum quos cupiditate porro alias ducimus facilis
-            quibusdam a similique quas quis enim deserunt, velit neque
-            reprehenderit excepturi.
-          </div>
+          <div className={style.cardsContainer}>cards : Mes décisions</div>
         </section>
         <section>
           <h2>Les décisions en cours</h2>
           <div className={style.cardsContainer}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nam
-            omnis tempore cum quos cupiditate porro alias ducimus facilis
-            quibusdam a similique quas quis enim deserunt, velit neque
-            reprehenderit excepturi.
+            cards : Les décisions en cours
           </div>
         </section>
         <section>
           <h2>Les décisions archivées</h2>
           <div className={style.cardsContainer}>
-            {/* map des decisions */}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nam
-            omnis tempore cum quos cupiditate porro alias ducimus facilis
-            quibusdam a similique quas quis enim deserunt, velit neque
-            reprehenderit excepturi.
+            cards : Les décisions archivées
           </div>
         </section>
         <button type="button" className={style.buttonScrollToTop}>
