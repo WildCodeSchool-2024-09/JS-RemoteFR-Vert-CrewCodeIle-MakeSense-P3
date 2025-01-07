c0ce6895d9f0fe4e4c72d4b0105861885165526c
Baczkiewicz Damien <baczkiewicz.damien@hotmail.fr>
2025-01-07 14:16:24 +0100

    fix: delete empty divs and style

--- client/src/pages/homePage/HomePage.tsx
+++ client/src/pages/homePage/HomePage.tsx
@@ -11,35 +11,22 @@ export default function HomePage() {
           <h2>Les décisions où je participe</h2>
           <div className={style.cardsContainer}>
             cards : Les décisions où je participe
-            <div className="container">
-              <div className="grid" />
-            </div>
-            ;
           </div>
         </section>
         <section>
           <h2>Mes décisions</h2>
           <div className={style.cardsContainer}>cards : Mes décisions</div>
-          <div className="container">
-            <div className="grid" />
-          </div>
         </section>
         <section>
           <h2>Les décisions en cours</h2>
           <div className={style.cardsContainer}>
             cards : Les décisions en cours
           </div>
-          <div className="container">
-            <div className="grid" />
-          </div>
         </section>
         <section>
           <h2>Les décisions archivées</h2>
           <div className={style.cardsContainer}>
             cards : Les décisions archivées
-          </div>
-          <div className="container">
-            <div className="grid" />
           </div>
         </section>
         <button type="button" className={style.buttonScrollToTop}>
