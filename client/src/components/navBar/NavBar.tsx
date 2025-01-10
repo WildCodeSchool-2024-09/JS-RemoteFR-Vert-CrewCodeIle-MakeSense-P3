442c91b37edc4a790c51af07e17a7aa66ddfeae2
Aurélien HAYE <aurelien.haye.techdeco@gmail.com>
2025-01-10 10:32:46 +0100

    fix: navbar

--- client/src/components/navBar/NavBar.tsx
+++ client/src/components/navBar/NavBar.tsx
@@ -14,89 +14,85 @@ function NavBar() {
   };
 
   return (
-      <nav className={style.navContainer}>
-        <NavLink to={"/homepage"}>
-          <img
-            src="/logo.png"
-            alt="Logo img"
-            className={style.logoImage}
-          />
-        </NavLink>
-        <ul className={style.navBarLink}>
-          <li>
-            <button
-              type="button"
-              className={style.dropdownButton}
-              onClick={toggleDropdown}
-            >
-              Les décisions
-            </button>
-            {isDropdownOpen && (
-              <ul className={style.dropdownMenu}>
-                <li>
-                  <button
-                    type="button"
-                    className={style.dropdownItemButton}
-                    onClick={closeDropdown}
-                  >
-                    Les décisions où je participe
-                  </button>
-                </li>
-                <li>
-                  <button
-                    type="button"
-                    className={style.dropdownItemButton}
-                    onClick={closeDropdown}
-                  >
-                    Mes décisions
-                  </button>
-                </li>
-                <li>
-                  <button
-                    type="button"
-                    className={style.dropdownItemButton}
-                    onClick={closeDropdown}
-                  >
-                    Les décisions en cours
-                  </button>
-                </li>
-                <li>
-                  <button
-                    type="button"
-                    className={style.dropdownItemButton}
-                    onClick={closeDropdown}
-                  >
-                    Les décisions archivées
-                  </button>
-                </li>
-                <li>
-                  <button
-                    type="button"
-                    className={style.dropdownItemButton}
-                    onClick={closeDropdown}
-                  >
-                    Toutes les décisions
-                  </button>
-                </li>
-              </ul>
-            )}
-          </li>
-          <li>
-            <button type="button" className={style.notificationButton}>
-              Notification
-            </button>
-          </li>
-          <li>
-            <NavLink to={"/profile"}>
-                <img
-                  src="/utilisateur.png"
-                  alt="Profil img"
-                  className={style.profileImage}
-                />
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <nav className={style.navContainer}>
+      <NavLink to={"/homepage"}>
+        <img src="/logo.png" alt="Logo img" className={style.logoImage} />
+      </NavLink>
+      <ul className={style.navBarLink}>
+        <li>
+          <button
+            type="button"
+            className={style.dropdownButton}
+            onClick={toggleDropdown}
+          >
+            Les décisions
+          </button>
+          {isDropdownOpen && (
+            <ul className={style.dropdownMenu}>
+              <li>
+                <button
+                  type="button"
+                  className={style.dropdownItemButton}
+                  onClick={closeDropdown}
+                >
+                  Les décisions où je participe
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className={style.dropdownItemButton}
+                  onClick={closeDropdown}
+                >
+                  Mes décisions
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className={style.dropdownItemButton}
+                  onClick={closeDropdown}
+                >
+                  Les décisions en cours
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className={style.dropdownItemButton}
+                  onClick={closeDropdown}
+                >
+                  Les décisions archivées
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className={style.dropdownItemButton}
+                  onClick={closeDropdown}
+                >
+                  Toutes les décisions
+                </button>
+              </li>
+            </ul>
+          )}
+        </li>
+        <li>
+          <button type="button" className={style.notificationButton}>
+            Notification
+          </button>
+        </li>
+        <li>
+          <NavLink to={"/profile"}>
+            <img
+              src="/utilisateur.png"
+              alt="Profil img"
+              className={style.profileImage}
+            />
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
