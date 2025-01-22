dc34b23246aefcdb3dd7ac13c1b892fb081a4051
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-01-22 20:04:19 +0100

    feat: parameters toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,24 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import { ToastContainer } from "react-toastify";
+import { Bounce, ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
       <Outlet />
-      <ToastContainer />
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
     </>
   );
 }
