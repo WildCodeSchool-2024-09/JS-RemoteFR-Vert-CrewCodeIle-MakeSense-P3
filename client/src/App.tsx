6efd2e9aec5846cd6c93e6cd8a76c7799458627f
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-01-22 19:54:17 +0100

    feat: install toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  return <Outlet />;
+  return (
+    <>
+      <Outlet />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
