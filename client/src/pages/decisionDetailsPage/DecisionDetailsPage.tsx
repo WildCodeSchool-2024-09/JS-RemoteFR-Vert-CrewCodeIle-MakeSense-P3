7e84fd75760ee20bc92bf2a3364b4149db7b8224
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-01-23 17:28:38 +0100

    fix: changes files

--- client/src/pages/decisionDetailsPage/DecisionDetailsPage.tsx
+++
@@ -1,9 +0,0 @@
-import DecisionDetails from "../../components/decisionDetails/DecisionDetails";
-
-export default function DecisionDetailsPage() {
-  return (
-    <main>
-      <DecisionDetails />
-    </main>
-  );
-}
