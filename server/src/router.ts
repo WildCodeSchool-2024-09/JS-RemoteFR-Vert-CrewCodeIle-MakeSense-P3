e184c8ad85bcaa5aa12c5de1e9c1b498c479fa77
Aurélien HAYE <aurelien.haye.techdeco@gmail.com>
2025-01-27 12:12:01 +0100

    feat: routes creation

--- server/src/router.ts
+++ server/src/router.ts
@@ -9,6 +9,21 @@ import UserRoute from "./routes/user.route";
 
 router.use("/", UserRoute);
 
+import countryActions from "./modules/country/countryAction";
+
+router.get("/api/country", countryActions.browse);
+router.get("/api/country/:id", countryActions.read);
+router.put("/api/country/:id", countryActions.edit);
+router.post("/api/country", countryActions.add);
+router.delete("/api/country/:id", countryActions.destroy);
+
+import roleActions from "./modules/role/roleAction";
+
+router.get("/api/role", roleActions.browse);
+router.get("/api/role/:id", roleActions.read);
+router.post("/api/role", roleActions.add);
+router.delete("/api/role/:id", roleActions.destroy);
+
 /* ************************************************************************* */
 
 export default router;
