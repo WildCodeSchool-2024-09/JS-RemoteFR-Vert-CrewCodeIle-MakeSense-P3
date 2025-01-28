37e61ac5ef9c88355144cf56a477513b8efaa94e
Aurélien HAYE <aurelien.haye.techdeco@gmail.com>
2025-01-28 14:40:47 +0100

    fix: schema

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -28,24 +28,6 @@ updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 
-CREATE TABLE IF NOT EXISTS category (
-id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-label VARCHAR(50) NOT NULL,
-created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-
-CREATE TABLE IF NOT EXISTS comment (
-id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-content TEXT NOT NULL,
-created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-user_id INTEGER NOT NULL,
-FOREIGN KEY (user_id) REFERENCES user(id)
-);
-
-
 CREATE TABLE IF NOT EXISTS decision (
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 title VARCHAR(255) NOT NULL,
@@ -60,14 +42,31 @@ created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 user_id INTEGER NOT NULL,
 FOREIGN KEY (user_id) REFERENCES user(id),
-category_id INTEGER NOT NULL,
-FOREIGN KEY (category_id) REFERENCES category(id),
-comment_id INTEGER NOT NULL,
-FOREIGN KEY (comment_id) REFERENCES comment(id),
 country_id INTEGER NOT NULL,
 FOREIGN KEY (country_id) REFERENCES country(id)
 );
 
+
+CREATE TABLE IF NOT EXISTS category (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+label VARCHAR(50) NOT NULL,
+decision_id INTEGER NOT NULL,
+FOREIGN KEY (decision_id) REFERENCES decision(id),
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS comment (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+content TEXT NOT NULL,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+user_id INTEGER NOT NULL,
+FOREIGN KEY (user_id) REFERENCES user(id),
+decision_id INTEGER NOT NULL,
+FOREIGN KEY (decision_id) REFERENCES decision(id)
+);
 
 CREATE TABLE IF NOT EXISTS vote (
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -96,3 +95,4 @@ FOREIGN KEY (vote_id) REFERENCES vote(id)
 
 INSERT INTO role (id,label) VALUES (1, 'applicant'), (2, 'user'), (3, 'administrator'), (4, 'rejected');
 INSERT INTO country (id,label) VALUES (1, 'France'), (2, 'Mexique'), (3, 'Canada'), (4, 'Pérou'), (5, 'Sénégal'), (6, 'Philippines'), (7, 'Liban'), (8, 'Cote d''Ivoire'), (9, 'Australie'), (10, 'Ukraine');
+INSERT INTO user (firstname, lastname, email, hash_password, avatar, role_id) VALUES ("Amandine", "Grard-Laurent", "a.grard@gmail.com", "12345", "avatar", 1 ), ( "Aurelien","vauchamp", "vauchamp@gmail.com", "123456", "avatar", 2), ( "lucie","vauchamp" , "lucie@gmail.com", "12345", "avatar", 2);
