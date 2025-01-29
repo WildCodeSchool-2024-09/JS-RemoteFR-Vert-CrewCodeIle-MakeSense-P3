7e717f8185978be4222b63c4b9eb10abdc724a63
Aurélien HAYE <aurelien.haye.techdeco@gmail.com>
2025-01-29 17:15:52 +0100

    fix: schema

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -5,7 +5,6 @@ created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-
 CREATE TABLE IF NOT EXISTS user (
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 firstname VARCHAR(50) NOT NULL,
@@ -19,14 +18,12 @@ role_id INTEGER NOT NULL DEFAULT 1,
 FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
-
 CREATE TABLE IF NOT EXISTS country (
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 label VARCHAR(150) NOT NULL,
 created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-
 
 CREATE TABLE IF NOT EXISTS decision (
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -46,12 +43,9 @@ country_id INTEGER NOT NULL,
 FOREIGN KEY (country_id) REFERENCES country(id)
 );
 
-
 CREATE TABLE IF NOT EXISTS category (
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 label VARCHAR(50) NOT NULL,
-decision_id INTEGER NOT NULL,
-FOREIGN KEY (decision_id) REFERENCES decision(id),
 created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -87,7 +81,6 @@ user_id INTEGER NOT NULL,
 FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
-
 CREATE TABLE IF NOT EXISTS user_vote_decision (
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 statut VARCHAR(50) NOT NULL,
@@ -101,7 +94,7 @@ vote_id INTEGER NOT NULL,
 FOREIGN KEY (vote_id) REFERENCES vote(id)
 );
 
-
 INSERT INTO role (id,label) VALUES (1, 'applicant'), (2, 'user'), (3, 'administrator'), (4, 'rejected');
 INSERT INTO country (id,label) VALUES (1, 'France'), (2, 'Mexique'), (3, 'Canada'), (4, 'Pérou'), (5, 'Sénégal'), (6, 'Philippines'), (7, 'Liban'), (8, 'Cote d''Ivoire'), (9, 'Australie'), (10, 'Ukraine');
 INSERT INTO user (firstname, lastname, email, hash_password, avatar, role_id) VALUES ("Amandine", "Grard-Laurent", "a.grard@gmail.com", "12345", "avatar", 1 ), ( "Aurelien","vauchamp", "vauchamp@gmail.com", "123456", "avatar", 2), ( "lucie","vauchamp" , "lucie@gmail.com", "12345", "avatar", 2);
+INSERT INTO decision (title,min_date,max_date,description,context,profit,risk,step,user_id,country_id) VALUES ("Title test", "2025-03-01", "2025-12-16", "Description test", "Context test", "Profit test", "Risk test", "Steps test", 1, 1), ("Title test 2", "2025-03-01", "2025-12-16", "Description test 2", "Context test", "Profit test", "Risk test", "Steps test", 1, 1);
