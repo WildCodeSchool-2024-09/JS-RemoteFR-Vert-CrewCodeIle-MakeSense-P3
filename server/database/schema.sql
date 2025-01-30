14995229c4a2b3c44c4310d218e4ee60432390d8
Aurélien HAYE <aurelien.haye.techdeco@gmail.com>
2025-01-30 11:26:08 +0100

    fix: schemaSQL

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -5,6 +5,13 @@ created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS country (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+label VARCHAR(150) NOT NULL,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS user (
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 firstname VARCHAR(50) NOT NULL,
@@ -14,15 +21,10 @@ hash_password VARCHAR(255) NOT NULL,
 avatar VARCHAR(255) NOT NULL,
 created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+country_id INTEGER NOT NULL,
+FOREIGN KEY (country_id) REFERENCES role(id),
 role_id INTEGER NOT NULL DEFAULT 1,
 FOREIGN KEY (role_id) REFERENCES role(id)
-);
-
-CREATE TABLE IF NOT EXISTS country (
-id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-label VARCHAR(150) NOT NULL,
-created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS decision (
@@ -81,20 +83,18 @@ user_id INTEGER NOT NULL,
 FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
-CREATE TABLE IF NOT EXISTS user_vote_decision (
+CREATE TABLE IF NOT EXISTS user_decision (
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-statut VARCHAR(50) NOT NULL,
+role VARCHAR(50) NOT NULL,
 created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 user_id INTEGER NOT NULL,
 FOREIGN KEY (user_id) REFERENCES user(id),
 decision_id INTEGER NOT NULL,
-FOREIGN KEY (decision_id) REFERENCES decision(id),
-vote_id INTEGER NOT NULL,
-FOREIGN KEY (vote_id) REFERENCES vote(id)
+FOREIGN KEY (decision_id) REFERENCES decision(id)
 );
 
 INSERT INTO role (id,label) VALUES (1, 'applicant'), (2, 'user'), (3, 'administrator'), (4, 'rejected');
 INSERT INTO country (id,label) VALUES (1, 'France'), (2, 'Mexique'), (3, 'Canada'), (4, 'Pérou'), (5, 'Sénégal'), (6, 'Philippines'), (7, 'Liban'), (8, 'Cote d''Ivoire'), (9, 'Australie'), (10, 'Ukraine');
-INSERT INTO user (firstname, lastname, email, hash_password, avatar, role_id) VALUES ("Amandine", "Grard-Laurent", "a.grard@gmail.com", "12345", "avatar", 1 ), ( "Aurelien","vauchamp", "vauchamp@gmail.com", "123456", "avatar", 2), ( "lucie","vauchamp" , "lucie@gmail.com", "12345", "avatar", 2);
+INSERT INTO user (firstname, lastname, email, hash_password, avatar, country_id, role_id) VALUES ("Amandine", "Grard-Laurent", "a.grard@gmail.com", "12345", "avatar", 1, 1 ), ( "Aurelien","vauchamp", "vauchamp@gmail.com", "123456", "avatar", 2, 2), ( "lucie","vauchamp" , "lucie@gmail.com", "12345", "avatar", 2, 2);
 INSERT INTO decision (title,min_date,max_date,description,context,profit,risk,step,user_id,country_id) VALUES ("Title test", "2025-03-01", "2025-12-16", "Description test", "Context test", "Profit test", "Risk test", "Steps test", 1, 1), ("Title test 2", "2025-03-01", "2025-12-16", "Description test 2", "Context test", "Profit test", "Risk test", "Steps test", 1, 1);
