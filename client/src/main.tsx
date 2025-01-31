dcff022ce5c4a92a8661a4b52b2d905ee682aff0
lea <monthieuxlea@gmail.com>
2025-01-31 11:21:12 +0100

    feat: sauvegarde

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,10 +7,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 // Import the main app component
 import App from "./App";
+import DecisionFormPage from "./pages/decisionFormPage/DecisionFormPage";
 import ErrorPage from "./pages/errorPage/ErrorPage";
 import HomePage from "./pages/homePage/HomePage";
 import RegisterPage from "./pages/registerPage/RegisterPage";
-import DecisionFormPage from "./pages/decisionFormPage/DecisionFormPage";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
