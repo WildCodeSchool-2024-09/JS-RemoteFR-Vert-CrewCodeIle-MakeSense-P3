ad058a5bc027161444239caf1862d0623fcab566
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-02-04 16:07:35 +0100

    feat: decision form

--- server/src/middlewares/generateColor.middleware.ts
+++
@@ -1,9 +0,0 @@
-// import type { RequestHandler } from "express";
-
-// function generateColor = (req : Request, res: Response, next: NextFunction) => {
-
-//     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6,'0');
-//     req.body.color = randomColor;
-//     next();
-// }
-// export default generateColor;
