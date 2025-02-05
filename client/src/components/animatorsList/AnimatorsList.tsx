f42c341b89c01e0c3c66f5248358c71b2b768708
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-02-05 09:04:06 +0100

    feat: decision_comment

--- client/src/components/animatorsList/AnimatorsList.tsx
+++ client/src/components/animatorsList/AnimatorsList.tsx
@@ -1,28 +1,27 @@
 // import { useEffect, useState } from "react";
 
-export default function AnimatorsList() {
-  //   const [animators, setAnimators] = useState([]);
+// export default function AnimatorsList({ id }) {
+//   const [animators, setAnimators] = useState([]);
 
-  //   useEffect(() => {
-  //     fetch(`${import.meta.env.VITE_API_URL}/api/user`)
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         setAnimators(data);
-  //       });
-  //   }, []);
+//   useEffect(() => {
+//     fetch(`${import.meta.env.VITE_API_URL}/api/user`)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setAnimators(data);
+//       });
+//   }, []);
 
-  return (
-    <></>
-    // animators.length > 0 && (
-    //   <section>
-    //     {animators.map((animator) => (
-    //       <article key={animator.id}>
-    //         <p>
-    //           {animator.firstname} {animator.lastname}
-    //         </p>
-    //       </article>
-    //     ))}
-    //   </section>
-    // )
-  );
-}
+//   return (
+//     animators.length > 0 && (
+//       <section>
+//         {animators.map((animator) => (
+//           <article key={animator.id}>
+//             <p>
+//               {animator.firstname} coucou{animator.lastname}
+//             </p>
+//           </article>
+//         ))}
+//       </section>
+//     )
+//   );
+// }
--- client/src/pages/decisionDetailPage/DecisionDetailPage.tsx
+++ client/src/pages/decisionDetailPage/DecisionDetailPage.tsx
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
-// import AnimatorsList from "../../components/animatorsList/AnimatorsList";
+
 import CommentsList from "../../components/commentsList/CommentsList";
 import DecisionDetail from "../../components/decisionDetail/DecisionDetail";
 import NavBar from "../../components/navBar/NavBar";
 import PostCommentDecision from "../../components/postCommentDecision.ts/PostCommentDecision";
+// import AnimatorsList from "../../components/animatorsList/AnimatorsList";
 
 export default function DecisionDetailPage() {
   const { id } = useParams<string>();
