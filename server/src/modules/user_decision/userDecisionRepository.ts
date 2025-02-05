c0041e81ae8fa02551bf032dc55a4c1a5b03ba25
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-02-05 09:40:06 +0100

    feat: animators

---
+++ server/src/modules/user_decision/userDecisionRepository.ts
@@ -0,0 +1,13 @@
+// import  databaseClient, { Rows }  from "../../../database/client";
+
+// class UserDecisionRepository(){
+
+// async read() {
+//     const [rows] = await databaseClient.query<Rows>(
+//         "SELECT user.*, decision.id FROM user JOIN decision ON decision"
+//     )
+// }
+
+// }
+
+// export default new UserDecisionRepository
